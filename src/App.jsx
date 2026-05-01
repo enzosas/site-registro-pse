b05da033a793c09d145c86707bec5ba00fcda866
Enzo Silveira <enzo.silveira2@gmail.com>
2026-05-01 14:52:33 -0300

    feat: adicionar mock selecao turma

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState } from 'react'
 
-export function IconeVoltar({ className, onClick }) {
+export function IconeVoltar({ onClick }) {
 	return (
 		<svg onClick={onClick} width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M20.625 11H1.375M1.375 11L11 20.625M1.375 11L11 1.375" stroke="#1E1E1E" stroke-width="2.75" stroke-linecap="round" stroke-linejoin="round" />
@@ -9,7 +9,7 @@ export function IconeVoltar({ className, onClick }) {
 	)
 }
 
-export function IconePesquisa({ className }) {
+export function IconePesquisa() {
 	return (
 		<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M12.8 12.8L9.90005 9.89999M11.4667 6.13332C11.4667 9.07884 9.0789 11.4667 6.13338 11.4667C3.18786 11.4667 0.800049 9.07884 0.800049 6.13332C0.800049 3.1878 3.18786 0.799988 6.13338 0.799988C9.0789 0.799988 11.4667 3.1878 11.4667 6.13332Z" stroke="#1E1E1E" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
@@ -62,7 +62,7 @@ function App() {
 										<input type="text" placeholder="Digite aqui para pesquisar" />
 										<IconePesquisa />
 									</div>
-									<div className='app--school-list'>
+									<div className='app--search-list'>
 										<p>Escola 1</p>
 										<p>Escola 2</p>
 										<p>Escola 3</p>
@@ -73,7 +73,7 @@ function App() {
 										<p>Escola 8</p>
 									</div>
 								</div>
-								
+
 								<div className='app--footer'>
 									<div className='app--buttonMain' onClick={avancar}>
 										<p>Avançar</p>
@@ -85,6 +85,20 @@ function App() {
 							<>
 								<IconeVoltar className="icone-voltar" onClick={voltar} />
 								<p className='app--title'>Selecione sua turma:</p>
+
+								<div className='app--search-container'>
+									<div className='app--search-bar'>
+										<input type="text" placeholder="Digite aqui para pesquisar" />
+										<IconePesquisa />
+									</div>
+									<div className='app--search-list'>
+										<p>Turma 21 2026</p>
+										<p>Turma 22 2026</p>
+										<p>Turma 31 2026</p>
+										<p>Turma 32 2026</p>
+										<p>Turma 33 2026</p>
+									</div>
+								</div>
 
 								<div className='app--footer'>
 									<div className='app--buttonMain' onClick={avancar}>
