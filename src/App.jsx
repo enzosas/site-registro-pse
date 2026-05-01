85ef0229d4e546373a3f1da2e37a2a76911e70ab
Enzo Silveira <enzo.silveira2@gmail.com>
2026-05-01 13:30:03 -0300

    feat: criar tela mock lista presenca

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,40 @@ function App() {
 
 	return (
 		<>
-			<p>hello!</p>
+			<div className='app--background'>
+				<div className='app--column'>
+					<p>Geração de Relatório</p>
+					<div className='app--card'>
+						<p className='app--title'>Defina a lista de presença:</p>
+						<div className='app--list'>
+                            <label>
+                                <input type="checkbox" />
+                                Aluno Importado 1
+                            </label>
+                            <label>
+                                <input type="checkbox" />
+                                Aluno Importado 2
+                            </label>
+                            <label>
+                                <input type="checkbox" />
+                                Aluno Importado 3
+                            </label>
+                            <label>
+                                <input type="checkbox" />
+                                Aluno Importado 4
+                            </label>
+                        </div>
+						<div className='app--footer'>
+							<div className='app--buttonSecondary'>
+								<p>Adicionar aluno manualmente</p>
+							</div>
+							<div className='app--buttonMain'>
+								<p>Avançar</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
