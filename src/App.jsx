4607aaa4ffd1d0603d3bb806be2eeaba721dcafa
Enzo Silveira <enzo.silveira2@gmail.com>
2026-05-08 17:44:04 -0300

    fix: ajustar esperssura check seleção

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export function IconePesquisa() {
 export function IconeCheck({ className }) {
 	return (
 		<svg className={className} width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M1.5 5.5L4.5 8.5L12.5 1.5" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+			<path d="M1.5 5.5L4.5 8.5L12.5 1.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
 		</svg>
 	)
 }
