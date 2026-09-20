05d372af2f2851644f5741c0449e9509f29d1015
Enzo Silveira <enzo.silveira2@gmail.com>
2026-09-20 08:42:37 -0300

    refactor: criar arquivo css proprio layout e botoes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/variables.css';
 import './styles/global.css';
+import './styles/layout.css';
+import './styles/buttons.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
